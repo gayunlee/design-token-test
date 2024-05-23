20c0abd662ba63faeec62dd29499a9761208c01d
gayunlee <gayunlee11@us-all.co.kr>
2024-05-23 08:57:04 +0000

    피그마 디자인 파일 변환

--- utilities.js
+++ utilities.js
@@ -1,41 +1,41 @@
 export const twUtils = {
   ".typography-H1-Bold": {
-    fontFamily: "Inter",
-    fontWeight: "Bold",
-    lineHeight: "110%",
-    fontSize: "78.126px",
-    paragraphSpacing: "32px",
-    letterSpacing: "-5%",
+    "fontFamily": "Inter",
+    "fontWeight": "Bold",
+    "lineHeight": "110%",
+    "fontSize": "78.126px",
+    "paragraphSpacing": "32px",
+    "letterSpacing": "-5%"
   },
   ".typography-H1-Regular": {
-    fontFamily: "Inter",
-    fontWeight: "Regular",
-    lineHeight: "110%",
-    fontSize: "78.126px",
-    paragraphSpacing: "32px",
-    letterSpacing: "-5%",
+    "fontFamily": "Inter",
+    "fontWeight": "Regular",
+    "lineHeight": "110%",
+    "fontSize": "78.126px",
+    "paragraphSpacing": "32px",
+    "letterSpacing": "-5%"
   },
   ".typography-H2-Bold": {
-    fontFamily: "Inter",
-    fontWeight: "Bold",
-    lineHeight: "110%",
-    fontSize: "39.063px",
-    paragraphSpacing: "26px",
-    letterSpacing: "-5%",
+    "fontFamily": "Inter",
+    "fontWeight": "Bold",
+    "lineHeight": "110%",
+    "fontSize": "39.063px",
+    "paragraphSpacing": "26px",
+    "letterSpacing": "-5%"
   },
   ".typography-H2-Regular": {
-    fontFamily: "Inter",
-    fontWeight: "Regular",
-    lineHeight: "110%",
-    fontSize: "39.063px",
-    paragraphSpacing: "26px",
-    letterSpacing: "-5%",
+    "fontFamily": "Inter",
+    "fontWeight": "Regular",
+    "lineHeight": "110%",
+    "fontSize": "39.063px",
+    "paragraphSpacing": "26px",
+    "letterSpacing": "-5%"
   },
   ".typography-Body": {
-    fontFamily: "Roboto",
-    fontWeight: "Regular",
-    lineHeight: "110%",
-    fontSize: "16px",
-    paragraphSpacing: "26px",
-  },
-};
+    "fontFamily": "Roboto",
+    "fontWeight": "Regular",
+    "lineHeight": "110%",
+    "fontSize": "16px",
+    "paragraphSpacing": "26px"
+  }
+}
