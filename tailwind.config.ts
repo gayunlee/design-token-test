6b178fd948db1934e25210a70ef60a38430612bc
gayunlee <gayunlee11@us-all.co.kr>
2024-05-23 11:55:47 +0900

    Chore:

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import { twVariables } from "./variableOutput";
 
 const config: Config = {
   content: [
@@ -8,10 +9,26 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      backgroundColor: {
+        ...twVariables.bg,
+      },
+      color: {
+        ...twVariables.colors,
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        ...twVariables.fontFamilies,
+      },
+      colors: {
+        "accent-1": "#FAFAFA",
+        "accent-2": "#EAEAEA",
+        "accent-7": "#333",
+        success: "#0070f3",
+        cyan: "#79FFE1",
       },
     },
   },
