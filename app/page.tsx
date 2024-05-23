99090b8d170615772c7f4af9df584f396d4f12e2
gayunlee <gayunlee11@us-all.co.kr>
2024-05-23 14:02:36 +0900

    add convertToTwVariables

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <main className="w-[300px] h-[300px] bg-muted">test</main>;
+  return (
+    <main className="w-[300px] h-[300px] bg-muted">
+      test
+      <div className="bg-white">
+        <h1 className="text-4xl font-extrabold text-gray-100">Text</h1>
+      </div>
+    </main>
+  );
 }
