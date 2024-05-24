548b649831485405cb3aca308f453744c4b92ae5
gayunlee <gayunlee11@us-all.co.kr>
2024-05-24 00:39:38 +0000

    피그마 디자인 파일 변환

--- utilities.js
+++ utilities.js
@@ -1,41 +1,5 @@
 export const twUtils = {
-  ".typography-H1-Bold": {
-    "fontWeight": "Bold",
-    "lineHeight": "110%",
-    "fontSize": "39.063px",
-    "paragraphSpacing": "32px",
-    "letterSpacing": "-5%",
-    "fontFamily": "Inter"
-  },
-  ".typography-H1-Regular": {
-    "fontWeight": "Regular",
-    "lineHeight": "110%",
-    "fontSize": "78.126px",
-    "paragraphSpacing": "32px",
-    "letterSpacing": "-5%",
-    "fontFamily": "600px"
-  },
-  ".typography-H2-Bold": {
-    "fontFamily": "Inter",
-    "fontWeight": "Bold",
-    "lineHeight": "110%",
-    "fontSize": "39.063px",
-    "paragraphSpacing": "26px",
-    "letterSpacing": "-5%"
-  },
-  ".typography-H2-Regular": {
-    "fontFamily": "Inter",
-    "fontWeight": "Regular",
-    "lineHeight": "110%",
-    "fontSize": "39.063px",
-    "paragraphSpacing": "26px",
-    "letterSpacing": "-5%"
-  },
-  ".typography-Body": {
-    "fontFamily": "Roboto",
-    "fontWeight": "Regular",
-    "lineHeight": "110%",
-    "fontSize": "16px",
-    "paragraphSpacing": "26px"
+  ".": {
+    "typography": "undefinedpx"
   }
 }
