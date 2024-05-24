36d375a1aed8035476bf65ceb1264b4ee47dc023
gayunlee <gayunlee11@us-all.co.kr>
2024-05-24 10:24:24 +0900

    dsdkfj

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="w-[300px] h-[300px] bg-surface-200">
-      <h1 className="text-secondary-200 heading-xl">Text</h1>
+    <main className="p-10 w-full h-[300px] bg-surface-200">
+      <h1 className="mb-3 text-primary-200 heading-xl-bold">Cupidatat</h1>
+      <h3 className="mb-10 text-secondary-200 heading-md-bold">
+        Est esse eiusmod consequat voluptate occaecat incididunt aliqua veniam
+        culpa Lorem aliquip elit proident nulla.
+      </h3>
+      <p>
+        In eu dolore non enim id ipsum elit culpa nulla amet tempor consectetur
+        aliquip. Excepteur pariatur proident id mollit. In do culpa tempor duis
+        cupidatat eu fugiat labore magna amet elit. Sint pariatur occaecat
+        commodo esse veniam occaecat ut duis fugiat et. Voluptate consectetur
+        culpa aute veniam ad cupidatat mollit cillum aliquip. Excepteur qui quis
+        fugiat excepteur nostrud cillum. Fugiat aliqua ullamco qui do ullamco
+        labore ut fugiat.
+      </p>
     </main>
   );
 }
